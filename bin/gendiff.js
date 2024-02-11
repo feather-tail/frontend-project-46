6888700e6b61d426172ad17f2420b5b8b3bddcfd
kaimoore <imeliaslake@gmail.com>
2024-02-11 19:24:10 +0300

    add jsons and update index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,14 @@
 #!/usr/bin/env node
 
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
 
 const program = new Command();
 
 program
   .version('0.1.0')
-  .description('Compares two configuration files and shows a difference.');
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => {});
 
 program.parse(process.argv);
