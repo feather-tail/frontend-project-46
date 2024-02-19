34fd2c25de6f9f6cfc8b6dc447ccf4dd0b790a57
Илья Рыжков <imeliaslake@gmail.com>
2024-02-19 15:47:29 +0300

    update parser.js

--- src/parser.js
+++ src/parser.js
@@ -4,10 +4,8 @@ const parser = (file) => {
 switch(getFormat(file)) {
   case 'json':
     return JSON.parse(readFile(file));
-    break;
   case 'yaml':
     return yaml.load(readFile(file));
-    break;
   default:
     throw new Error('This format is not supported');
 }
