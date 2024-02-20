e6d8bd1b3d3e6734ca87b0210ccae85ef4816e3b
kaimoore <imeliaslake@gmail.com>
2024-02-20 21:52:27 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import fs from 'fs';
 import path from 'path';
 const process = require('process');
+import parseFile from './parser.js';
 
 const getAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const readFile = (filepath) => fs.readFileSync(getAbsolutPath(filepath), 'utf-8');
 const getFormat = (filename) => filename.split('.')[1];
+
+const getDiff = (objectOne, objectTwo) => {
+  const result = {};
+  return result;
+};
 
 export { getAbsolutePath, readFile, getFormat };
--- src/parser.js
+++ src/parser.js
@@ -1,14 +1,18 @@
 import yaml from 'js-yaml';
+import { getFormat } from '.';
+import _ from 'lodash';
 
-const parser = (file) => {
-switch(getFormat(file)) {
-  case 'json':
-    return JSON.parse(readFile(file));
-  case 'yaml':
-    return yaml.load(readFile(file));
-  default:
-    throw new Error('This format is not supported');
-}
+const parseFile = (filename) => {
+  switch(getFormat(filename)) {
+    case 'json':
+      return [JSON.parse(readFile(filename))];
+    case 'yaml':
+      return [yaml.load(readFile(filename))];
+    default:
+      throw new Error('This format is not supported');
+  }
 };
 
-export default parser;
+const getSortedFile = (filename) => _.sortBy(parseFile(filename));
+
+export { parseFile, getSortedFile };
