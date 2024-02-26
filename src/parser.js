13ef95f767ed9f696094fa49fff94fe3229e2817
Илья Рыжков <imeliaslake@gmail.com>
2024-02-26 12:31:25 +0300

    fix names

--- src/parser.js
+++ src/parser.js
@@ -4,9 +4,9 @@ import { getFormat, readFile } from './index.js';
 const parseFile = (filename) => {
   switch(getFormat(filename)) {
     case 'json':
-      return [JSON.parse(readFile(filename))];
+      return JSON.parse(readFile(filename));
     case 'yaml':
-      return [yaml.load(readFile(filename))];
+      return yaml.load(readFile(filename));
     default:
       throw new Error('This format is not supported');
   }
