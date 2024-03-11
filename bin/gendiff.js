18b1ff9e99397d769b3547ee7dd0ba11caf2aa24
Илья Рыжков <imeliaslake@gmail.com>
2024-03-11 11:09:01 +0300

    update fixtures

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,7 +8,7 @@ const program = new Command();
 program
   .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
+  .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filepath1, filepath2) => {
     console.log(getDiff(filepath1, filepath2));
