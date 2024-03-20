895b53b6de79edbcc0ca66f1089cbd30dc763f46
Илья Рыжков <imeliaslake@gmail.com>
2024-03-20 15:17:06 +0300

    update tests

--- __tests__/index.tests.js
+++ __tests__/index.tests.js
@@ -7,7 +7,7 @@ import getDiff from '../src/index.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-test('common case JSON', () => {
+test('common case JSON with stylish format', () => {
   const firstFileJSON = `${__dirname}/../__fixtures__/file1.json`;
   const secondFileJSON = `${__dirname}/../__fixtures__/file2.json`;
   const expectedDiffJSON = fs.readFileSync(
@@ -15,4 +15,34 @@ test('common case JSON', () => {
     'utf-8',
   );
   expect(getDiff(firstFileJSON, secondFileJSON, 'stylish')).toEqual(expectedDiffJSON);
+});
+
+test('common case YML with stylish format', () => {
+  const firstFileYML = `${__dirname}/../__fixtures__/file1.yml`;
+  const secondFileYML = `${__dirname}/../__fixtures__/file2.yml`;
+  const expectedDiffYML = fs.readFileSync(
+    `${__dirname}/../__tests__/expectedStylishResult.txt`,
+    'utf-8',
+  );
+  expect(getDiff(firstFileYML, secondFileYML, 'stylish')).toEqual(expectedDiffYML);
+});
+
+test('common case JSON with plain format', () => {
+  const firstFileJSON = `${__dirname}/../__fixtures__/file1.json`;
+  const secondFileJSON = `${__dirname}/../__fixtures__/file2.json`;
+  const expectedDiffJSON = fs.readFileSync(
+    `${__dirname}/../__tests__/expectedPlainResult.txt`,
+    'utf-8',
+  );
+  expect(getDiff(firstFileJSON, secondFileJSON, 'plain')).toEqual(expectedDiffJSON);
+});
+
+test('common case YML with plain format', () => {
+  const firstFileYML = `${__dirname}/../__fixtures__/file1.yml`;
+  const secondFileYML = `${__dirname}/../__fixtures__/file2.yml`;
+  const expectedDiffYML = fs.readFileSync(
+    `${__dirname}/../__tests__/expectedPlainResult.txt`,
+    'utf-8',
+  );
+  expect(getDiff(firstFileYML, secondFileYML, 'plain')).toEqual(expectedDiffYML);
 });
