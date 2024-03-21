976c00fbce1f4724ce5eee3d25d15b44f24b317e
Илья Рыжков <imeliaslake@gmail.com>
2024-03-21 13:56:06 +0300

    update gendiff.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 
 import { Command } from 'commander';
-import path from 'path';
 import getDiff from '../src/index.js';
 
 const program = new Command();
@@ -12,9 +11,7 @@ program
   .option('-f, --format [type]', 'output format', 'stylish')
   .arguments('<filepath1> <filepath2>')
   .action((filename1, filename2, options) => {
-    const file1Path = path.resolve(process.cwd(), '__fixtures__', filename1);
-    const file2Path = path.resolve(process.cwd(), '__fixtures__', filename2);
-    console.log(getDiff(file1Path, file2Path, options.format));
+    console.log(getDiff(filename1, filename2, options.format));
   });
 
 program.parse(process.argv);
